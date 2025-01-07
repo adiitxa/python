655af212307c2820ecb7d4cb057f5a129d205800
Adi000012 <adityabgaikwad0012@gmail.com>
2025-01-07 18:48:57 +0530

    updated code

---
+++ 4_operators_in_python.py
@@ -0,0 +1,51 @@
+#Arithmatic operator
+'''
++	Addition	            5 + 3 → 8
+-	Subtraction	            5 - 3 → 2
+*	Multiplication	        5 * 3 → 15
+/	Division	            5 / 2 → 2.5
+//	Floor Division	        5 // 2 → 2
+%	Modulus (remainder)	    5 % 2 → 1
+**	Exponentiation	        2 ** 3 → 8 '''
+
+#Relational Operaror
+'''
+==	Equal to	                5 == 5 → True
+!=	Not equal to	            5 != 3 → True
+>	Greater than	            5 > 3 → True
+<	Less than	                5 < 3 → False
+>=	Greater than or equal to	5 >= 3 → True
+<=	Less than or equal to	    5 <= 3 → False'''
+
+
+#Logical operator
+'''
+and 
+or 
+not '''
+
+#Bitwise operator
+'''
+&	AND	            5 & 3 → 1
+`	`	            OR
+^	XOR	            5 ^ 3 → 6
+~	NOT	            ~5 → -6
+<<	Left shift	    5 << 1 → 10
+>>	Right shift	    5 >> 1 → 2'''
+
+
+#membership operators
+'''
+1) in
+2) not in
+'''
+
+fruits = ["apple", "banana", "cherry"]
+print("apple" in fruits)    # True
+print("grape" not in fruits) # True
+
+#identify Operator
+'''
+1) is
+2) is not 
+'''

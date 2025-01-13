cc195bf806c85755c25a41d5a708d57f09f6f1b9
Adi000012 <adityabgaikwad0012@gmail.com>
2025-01-13 23:46:23 +0530

    updated code

---
+++ 12_setOperation.py
@@ -0,0 +1,9 @@
+# Set with unique elements
+my_set = {1, 2, 3, 4}
+
+# Using the set constructor
+my_set = set([1, 2, 2, 3, 4])  # Output: {1, 2, 3, 4}
+
+my_set = {1, 2, 3}
+for item in my_set:
+    print(item)
--- 9_tuplesOperatin.py
+++ 9_tuplesOperatin.py
@@ -1,5 +1,16 @@
 #tuples are immuatable
 
+'''
+count(value)	        Counts occurrences of value.
+index(value)	        Returns the first index of value.
+len()	                Returns the length of the tuple.
++	                    Concatenates tuples.
+*	                    Repeats the tuple.
+in	                    Checks for membership.
+for loop	            Iterates through tuple elements.
+sorted()	            Returns a sorted list of tuple elements.
+'''
+
 #create tuple with single element
 tuple1 = (1, )
 
@@ -12,5 +23,19 @@ print(tuple2[2])
 #slicing
 print(tuple2[1:4])
 
-t = (1, 2, 2, 3, 4, 2)
-print(t.count(2))
+t1 = (1, 2, 2, 3, 4, 2)
+print(t1.count(2))
+
+t2 = (10, 20, 30, 40)
+print(t2.index(30))
+
+#concact 2 list
+print(t1 + t2)  
+
+#list multiply 3 copy
+print(t1 * 3)
+
+#nested tuples
+nested_tuple = (1, (2, 3), (4, 5, 6))
+print(nested_tuple[1])  
+print(nested_tuple[1][0])  

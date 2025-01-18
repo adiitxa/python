375f66665c693b0f9946150896a3a27d6b46f005
Adi000012 <adityabgaikwad0012@gmail.com>
2025-01-19 02:33:56 +0530

    updated code

---
+++ 14_function_Python.py
@@ -0,0 +1,21 @@
+#simple function for adding
+
+def functionAdd(a, b):
+    result= a+b
+    print(result)
+
+functionAdd(3,4)
+
+
+#in this case function return a value
+def functionMul(a, b):
+    return a*b
+
+mul = functionMul(3,4)
+print(mul)
+
+
+#body after some time it just passing
+def functionSub(a, b):
+    pass
+
---
+++ 15_docStrings.py
@@ -0,0 +1,10 @@
+#docStrings is for understagings 
+
+def functionSquare():
+    '''
+    hey my name is function 1
+    '''
+    print("hello")
+
+functionSquare()
+print(functionSquare.__doc__)

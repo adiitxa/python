5da24b1da1a3f5cc92a865835e6910e15320d022
Adi000012 <adityabgaikwad0012@gmail.com>
2025-01-31 23:43:50 +0530

    updated code

---
+++ 23_lambdaFunction.py
@@ -0,0 +1,16 @@
+def function1(x):
+    return x + 10
+
+print(function1(10))
+
+
+func = lambda x: x+10
+print(func(10))
+
+
+multiply = lambda a, b : a * b
+print(multiply(23, 54))
+
+#conditional statement
+ifElse = lambda c, d : c if c > d else 0
+print(ifElse(2,5))
